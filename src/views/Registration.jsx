c80cbe24309fda869b1c3b09ae361208abd7575f
Laura <laurafcanon@gmail.com>
2022-01-12 20:08:56 -0500

    Modify registration/add form validation

--- src/views/Registration.jsx
+++ src/views/Registration.jsx
@@ -1,90 +1,154 @@
 import "./allViews.css";
+import { useState, useEffect } from "react";
 import React from "react";
 
 export default function HomeCollaborator() {
+  const initialValues = {name:"", lastName:"", email:"", password:""}
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormValues({...formValues, [name]: value});
+    console.log(formValues);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  }
+  useEffect(() => {
+    console.log(formErrors)
+    if(Object.keys(formErrors).length === 0 && isSubmit){
+      console.log(formValues);
+    }
+  }, [formErrors])
+  const validate = (values) => {
+    const errors = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if(!values.name) {
+      errors.name = "El nombre es requerido!"
+    }
+    if(!values.lastName) {
+      errors.lastName = "El apellido es requerido!"
+    } 
+    if(!values.email) {
+      errors.email = "El email es requerido!"
+    } else if (!regex.test(values.email)) {
+      errors.email = "Introduce una dirección de correo electrónico válida"
+    }
+    if(!values.password) {
+      errors.password = "La contraseña es requerida!"
+    } else if(values.password.length < 4) {
+      errors.password = "La contrasena debe tener más de 4 caracteres"
+    }
+    return errors;
+  }
   return (
-    <main className="mt-5 container py-5 ">
-      <div className="row mt-5">
-        <div className="col">
-          <h3>Registrate aqui</h3>
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="InputNombre"
-              placeholder="Nombre"
-            />
+    <main className="mt-5 container py-5">
+      <div className="row display-flex mt-3 justify-content-center">
+        <div className="col-md-6 col-lg-7">
+            <h2 className="text-center mb-3">Unete a la comunidad FIXHOGAR!</h2>
+            <div className="text-center mb-3">
+              <img
+                src="https://image.freepik.com/vector-gratis/herramientas-mejora-hogar_24640-74868.jpg"
+                style={{width:"80%"}}
+                alt="home-work"
+              />
+            </div>
           </div>
+        <div className="col-md-6 col-sm-11 col-lg-5">
+          <h2 className="mb-5 text-center">Regístrate</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control mb-2"
+                name="name"
+                id="InputNombre"
+                placeholder="Nombre"
+                value= {formValues.name}
+                onChange= {handleChange}
+              />
+            </div>
+            <p className="text-danger">{formErrors.name}</p>
 
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="InputApellido"
-              placeholder="Apellido"
-            />
-          </div>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control mb-2"
+                name="lastName"
+                id="InputApellido"
+                placeholder="Apellido"
+                value= {formValues.lastName}
+                onChange= {handleChange}
+              />
+            </div>
+            <p className="text-danger">{formErrors.lastName}</p>
 
-          <div className="input-group mb-3">
-            <input
-              type="email"
-              className="form-control"
-              id="InputCorreo"
-              placeholder="Correo electronico"
-            />
-          </div>
+            <div className="input-group mb-3">
+              <input
+                type="email"
+                className="form-control mb-2"
+                name="email"
+                id="InputCorreo"
+                placeholder="Correo electronico"
+                value= {formValues.email}
+                onChange= {handleChange}
+              />
+            </div>
+            <p className="mt-1 text-danger">{formErrors.email}</p>
 
-          <div className="input-group mb-3">
-            <input
-              type="password"
-              className="form-control"
-              id="InputContrasena"
-              placeholder="Contrasena"
-            />
-          </div>
+            <div className="input-group mb-3">
+              <input
+                type="password"
+                className="form-control mb-2"
+                name="password"
+                id="InputContrasena"
+                placeholder="Contrasena"
+                value= {formValues.password}
+                onChange= {handleChange}
+              />
+            </div>
+            <p className="mt-1 text-danger">{formErrors.password}</p>
 
-          <div className="form-check mt-5">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="CheckTerminos"
-            />
-            <label className="form-check-label" for="CheckTerminos">
-              He leído y acepto los términos y condiciones
-            </label>
+            <div className="form-check mt-3">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="CheckTerminos"
+              />
+              <label className="form-check-label" for="CheckTerminos">
+                He leído y acepto los términos y condiciones
+              </label>
+            </div>
+            <div className="form-check mt-2">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="CheckDatos"
+              />
+              <label className="form-check-label" for="CheckDatos">
+                He leído y acepto la política de privacidad de datos
+              </label>
+            </div>
+            <div className="mt-3 text-center">
+              <button type="submit" className="btn btn-outline-primary">
+                Registrarse
+              </button>
+            </div>
+          </form>
+          <div className="mt-4">
+            <p className="text-center">Ya tienes una cuenta?</p>
+            <div className="text-center mb-3">
+            <a href="/sessionlogin">Inicia Sesión</a>
           </div>
-          <div className="form-check mt-2">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="CheckDatos"
-            />
-            <label className="form-check-label" for="CheckDatos">
-              He leído y acepto la política de privacidad de datos
-            </label>
-          </div>
-          <div className="mt-3 offset-4">
-            <button type="submit" className="btn btn-outline-primary">
-              Registrarse
-            </button>
-          </div>
-        </div>
-        <div className="col">
-          <div className="d-flex" style={{ height: "600px" }}>
-            <div className="vr"></div>
-          </div>
-        </div>
-        <div className="col">
-          <h1>Se parte de la comunidad XXXX!</h1>
-          <div>
-            <img
-              src="images/image1.jpg"
-              alt="No se que imagen se supone va aqui"
-            />
-          </div>
-        </div>
+        </div>     
+      </div>
       </div>
     </main>
   );
