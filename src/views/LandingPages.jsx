4545d86ad475da3536bf5e9d803e530f10899429
Andres Puello <apuello1025@gmail.com>
2022-01-24 19:22:28 -0500

    Se agrego la funcion de enviar a la URL search de acuerdo a los Select validados

--- src/views/LandingPages.jsx
+++ src/views/LandingPages.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function LandingPages() {
+  const [values, setValues] = useState({
+    service: 'none',
+    city: 'none',
+    price: '0',
+  });
+
+  const valueHandlerService = (event) => {
+    setValues((prevValues) => ({ ...prevValues, service: event.target.value }));
+  };
+  const valueHandlerCity = (event) => {
+    setValues((prevValues) => ({ ...prevValues, city: event.target.value }));
+  };
+  const valueHandlerPrice = (event) => {
+    setValues((prevValues) => ({ ...prevValues, price: event.target.value }));
+  };
+
+  const alertButton = () => {
+    return alert('Seleccione todos los campos para realizar la busqueda');
+  };
   return (
     <div>
       <main className="flex-shrink-0 container">
@@ -13,20 +32,20 @@ export default function LandingPages() {
           <div className="carousel-inner">
             <div
               className="carousel-item image-cover active"
-              style={{ backgroundImage: "url(../img/232641266.jpg)" }}
+              style={{ backgroundImage: 'url(../img/232641266.jpg)' }}
             ></div>
             <div
               className="carousel-item image-cover"
               style={{
                 backgroundImage:
-                  "url(../img/funny-electrician-installing-plug-with-screwdriver-carrying-mouth.jpg)",
+                  'url(../img/funny-electrician-installing-plug-with-screwdriver-carrying-mouth.jpg)',
               }}
             ></div>
             <div
               className="carousel-item image-cover"
               style={{
                 backgroundImage:
-                  "url(../img/repairman-doing-air-conditioner-service.jpg)",
+                  'url(../img/repairman-doing-air-conditioner-service.jpg)',
               }}
             ></div>
           </div>
@@ -56,48 +75,76 @@ export default function LandingPages() {
           </button>
         </div>
         <div className="container-fluid p-2 border-2 bg-black bg-opacity-25 mt-2">
-          <form className="d-flex" >
+          <form className="d-flex">
             <select
               className="form-select m-1 shadow-sm"
               aria-label="Default select example"
+              value={values.service}
+              onChange={valueHandlerService}
             >
-              <option selected disabled>
+              <option selected value="none" disabled>
                 Selecciona un servicio:
               </option>
-              <option value="1">Instalación TV</option>
-              <option value="2">Instalación Nevera</option>
-              <option value="3">Carpintería</option>
-              <option value="4">Plomería</option>
-              <option value="5">Pintura de Interiores</option>
-              <option value="6">Mantenimiento Lavadora</option>
-              <option value="7">Jardinería</option>
-              <option value="8">Reparación de Tuberías</option>
+              <option value="installTV">Instalación TV</option>
+              <option value="installFrezzer">Instalación Nevera</option>
+              <option value="carpenter">Carpintería</option>
+              <option value="plomery">Plomería</option>
+              <option value="interiorPaint">Pintura de Interiores</option>
+              <option value="washinMachineMaint">Mantenimiento Lavadora</option>
+              <option value="gardening">Jardinería</option>
             </select>
             <select
               name="ciudades"
               className="form-select m-1"
               aria-label="Default select example"
+              value={values.city}
+              onChange={valueHandlerCity}
             >
-              <option selected disabled>
+              <option selected value="none" disabled>
                 Selecciona tu ciudad
               </option>
             </select>
             <select
               className="form-select m-1"
               aria-label="Default select example"
+              value={values.price}
+              onChange={valueHandlerPrice}
             >
-              <option selected disabled>
+              <option selected value="0" disabled>
                 Rango de Precios:
               </option>
               <option value="1">$50.000 - $100.000</option>
               <option value="2">$100.001 - $200.000</option>
               <option value="3">$200.001 - $300.000</option>
             </select>
-            <Link to='/search' >
-              <button className="btn btn-dark m-1" type="submit">
-                Search
-              </button>
-            </Link>
+            {values.service === 'none' ? (
+              <>
+                <button className="btn btn-dark m-1" onClick={alertButton}>
+                  Search
+                </button>
+              </>
+            ) : values.city === 'none' ? (
+              <>
+                <button className="btn btn-dark m-1" onClick={alertButton}>
+                  Search
+                </button>
+              </>
+            ) : values.price === '0' ? (
+              <>
+                <button className="btn btn-dark m-1" onClick={alertButton}>
+                  Search
+                </button>
+              </>
+            ) : (
+              <>
+                <Link
+                  className="btn btn-dark m-1"
+                  to={`/search?service=${values.service}&?city=${values.city}&?price=${values.price}`}
+                >
+                  Search
+                </Link>
+              </>
+            )}
           </form>
         </div>
       </main>
