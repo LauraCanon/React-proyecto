88c57c641abbe1acacd68380dba273775c0da6ea
Laura <laurafcanon@gmail.com>
2022-02-07 08:49:32 -0500

    Adds minor changes

--- src/services/fixHogarApi/fixHogarApi.js
+++ src/services/fixHogarApi/fixHogarApi.js
@@ -7,29 +7,29 @@ axios.defaults.baseURL = "http://localhost:3000";
 
 export const fixHogarApi = {
   async loginUser(user) {
-    const config = { header: { 'Content-type': 'application/json' } };
-    const response = await axios.post('/sessionlogin', user, config);
-    console.log(response);
+    const config = { header: { "Content-type": "application/json" } };
+    const response = await axios.post("/sessionlogin", user, config);
+    console.log("llego aqui", response);
     // token = response.data.token;
     return response.data;
   },
   async collabRegister(collabs) {
-    const config = { header: { 'Content-type': 'application/json' } };
-    const response = await axios.post('/collabregister', collabs, config);
+    const config = { header: { "Content-type": "application/json" } };
+    const response = await axios.post("/collabregister", collabs, config);
     console.log(response);
     return response.data;
   },
   async userRegister(users) {
     try {
-      const config = { header: { 'Content-type': 'application/json' } };
-      const response = await axios.post('/useregister', users, config);
+      const config = { header: { "Content-type": "application/json" } };
+      const response = await axios.post("/useregister", users, config);
       console.log(response);
       return response.data;
     } catch (error) {
       console.log(error.request);
     }
   },
-   async verifiedEmail(id, hash) {
+  async verifiedEmail(id, hash) {
     console.log(hash);
     try {
       const config = { header: { "Content-type": "application/json" } };
@@ -42,7 +42,7 @@ export const fixHogarApi = {
   },
   async servicesCollabs() {
     try {
-      const response = await axios.get('/services');
+      const response = await axios.get("/services");
       // console.log(response)
       return response.data;
     } catch (error) {
@@ -51,7 +51,7 @@ export const fixHogarApi = {
   },
   async citysCollabs(service) {
     try {
-      const response = await axios.get('/city', { params: { service } });
+      const response = await axios.get("/city", { params: { service } });
       // console.log(response.data);
       return response.data;
     } catch (error) {
@@ -61,10 +61,19 @@ export const fixHogarApi = {
   async serviceCollabList(search) {
     const { service, city } = search;
     try {
-      const response = await axios.get('/search/services', {
+      const response = await axios.get("/search/services", {
         params: { service, city },
       });
       console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error.response);
+    }
+  },
+  async fileUser() {
+    try {
+      const response = await axios.get("/file/user");
+      console.log(response);
       return response.data;
     } catch (error) {
       console.log(error.response);
---
+++ src/store/userSlicer/fileUserSlicer.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fixHogarApi } from "../../services/fixHogarApi/fixHogarApi";
+
+i; //Thunk actions
+export const fileUser = createAsyncThunk("user/fileUser", (user) =>
+  fixHogarApi.fileUser(user)
+);
+
+const initialState = {
+  user: [],
+};
+const collabSlicer = createSlice({
+  name: "user",
+  initialState: initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fileUser.pending, (state) => {})
+      .addCase(fileUser.fulfilled, (state, action) => {
+        state.user = true;
+      })
+      .addCase(fileUser.rejected, (state) => {});
+  },
+});
